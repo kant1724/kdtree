2f0aeb48d0d92e3b6bbcba3c384fc0e99db98ed8
kant1724 <sonseungbum83@gmail.com>
2019-07-21 11:08:40 +0900

    Revert "commit"
    This reverts commit 5b52d971

--- extract_vectors.py
+++ extract_vectors.py
@@ -1,19 +1,8 @@
-import numpy as np
+words = {}
 
-words = {
-    '연필': True,
-    '딸기': True,
-    '자동차': True,
-    '하드웨어': True,
-    '과일': True,
-    '사무용품': True,
-    '가전기기': True,
-    '청소기': True,
-    '컴퓨터': True,
-    '수박': True,
-    '사과': True,
-    '종이': True
-}
+with open('./word_list.txt', encoding='utf8') as f:
+    for line in f:
+        words[line.replace('\n', '')] = True
 
 with open('./cc.ko.300.vec', encoding='utf8', errors='ignore') as f:
     with open('./my.ko.300.vec', 'w', encoding='utf8') as fw:
